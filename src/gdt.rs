a224c627956b03b53a502a4539c5d70f8864b23a
yotiosoft <ytani0323@gmail.com>
2026-02-21 10:57:52 +0900

    user-mode-implementation: TSS privilege_stack_table

--- src/gdt.rs
+++ src/gdt.rs
@@ -9,6 +9,7 @@ pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
 lazy_static! {
     static ref TSS: TaskStateSegment = {
         let mut tss = TaskStateSegment::new();
+        
         tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
             const STACK_SIZE: usize = 4096 * 5;
             static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
@@ -17,6 +18,16 @@ lazy_static! {
             let stack_end = stack_start + STACK_SIZE;
             stack_end
         };
+        
+        tss.privilege_stack_table[0] = {
+            const STACK_SIZE: usize = 4096 * 5;
+            static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
+
+            let stack_start = VirtAddr::from_ptr(&raw const STACK);
+            let stack_end = stack_start + STACK_SIZE;
+            stack_end
+        };
+        
         tss
     };
 }
