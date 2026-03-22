dc569dba9e7c21f2dc1cb272d9830b835502c2f8
yotiosoft <ytani0323@gmail.com>
2026-03-22 22:07:39 +0900

    remove old sh scripts

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-cargo build --release
-IMG=$(find target/x86_64-unknown-none/release/build/ferrios-*/out/bios.img 2>/dev/null | head -1)
-cp "$IMG" target/bios.img
-echo "bios.img -> target/bios.img"
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-ARGS="$@"
-
-./build.sh
-qemu-system-x86_64 -drive format=raw,file=target/bios.img $ARGS
