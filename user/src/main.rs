1dda8d16c53b96ba4ff0cb76abbb91d3cd16e570
yotiosoft <ytani0323@gmail.com>
2026-04-06 00:38:57 +0900

    user: run syscall via asm

--- user/src/main.rs
+++ user/src/main.rs
@@ -1,18 +1,27 @@
 #![no_std]
 #![no_main]
 
+use core::arch::asm;
 use core::panic::PanicInfo;
+
+const SYS_PRINT_NUM: u64 = 0;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    loop {
-        core::hint::spin_loop();
+    loop { 
+        unsafe {
+            asm!(
+                "syscall",
+                in("rax") SYS_PRINT_NUM,
+                in("rdi") 123u64,
+                lateout("rcx") _,
+                lateout("r11") _,
+            );
+        }
     }
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {
-        core::hint::spin_loop();
-    }
+    loop { core::hint::spin_loop(); }
 }
